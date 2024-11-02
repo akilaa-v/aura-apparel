06212208b015d041453183581b99677f61eb2908
akilaa-v <akilaavignesh1116@gmail.com>
2024-11-02 23:53:50 +0530

    Added website logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,12 @@ import Cart from "./Cart";
 import SearchBar from "./SearchBar";
 import "./Header.css";
 import User from "./User";
-
+import logo from "../../assets/201975000.png"
 const Header = () => {
 	return (
 		<header className="header-container">
-			<h1 className="website-name">Aura Apparel</h1>
+			{/* <h1 className="website-name">Aura Apparel</h1> */}
+			<img src={logo} alt="website logo" className="website-logo" />
 			<SearchBar />
 			<User/>
 			<Cart />
